15c2812b944a24110b72df64f71941c48114cdc8
Geetha1303 <geetha@zelarsoft.com>
2021-07-20 12:14:04 +0530

    `latest`

--- Instance-create/create.sh
+++ Instance-create/create.sh
@@ -14,7 +14,7 @@ Instance_Create() {
   COMPONENT=$1
   INSTANCE_EXISTS=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${COMPONENT}  | jq .Reservations[])
   STATE=$(aws ec2 describe-instances     --filters Name=tag:Name,Values=${COMPONENT}  | jq .Reservations[].Instances[].State.Name | xargs)
-  if [ -z "${INSTANCE_EXISTS}" -o "$STATE" == "stopped"  ]; then
+  if [ -z "${INSTANCE_EXISTS}" -o "$STATE" == "terminated"  ]; then
     aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}, {Key=Project,Value=TODO}]" | jq
   else
     echo "Instance ${COMPONENT} already exists"
@@ -23,8 +23,8 @@ Instance_Create() {
 
   IPADDRESS=$(aws ec2 describe-instances     --filters Name=tag:Name,Values=${COMPONENT}   | jq .Reservations[].Instances[].PrivateIpAddress | grep -v null |xargs)
 
-  #sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${IPADDRESS}/" record.json >/tmp/record.json
- # aws route53 change-resource-record-sets --hosted-zone-id Z01109932OAEV19U6FDDX --change-batch file:///tmp/record.json
+  sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${IPADDRESS}/" record.json >/tmp/record.json
+  aws route53 change-resource-record-sets --hosted-zone-id Z07549851GV8ILAQM5W75 --change-batch file:///tmp/record.json
   sed -i -e "/${COMPONENT}/ d" ../inventory
   echo "${IPADDRESS} COMPONENT=$(echo ${COMPONENT} | awk -F - '{print $1}')" >>../inventory
 }
